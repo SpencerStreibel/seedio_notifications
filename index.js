2f03d705e12f288be59f873c3363d1cc01d02024
spencer <spencer@livioradio.com>
2015-11-12 16:31:35 -0500

    Barely functioning.

--- index.js
+++ index.js
@@ -3,27 +3,31 @@ var express = require('express'),
     mongoose = require('mongoose'),
     mandrill = require('mandrill-api/mandrill'),
     mandrill_client = new mandrill.Mandrill('jx5mDqtrFCdoAkM4o60F-A'), /*or new API key*/
+    port = 4242
     params = {
       "message": {
-        "from_email": /*"livio@livio.io"*/,
-        "to": [{"email":/*"request@email.com"*/}],
-        "subject": /*"Notification Subject"*/,
-        "text": /*"Notification body."*/
+        "from_email": "spencer@livioradio.com",
+        "to": [{"email":"spencer@livioradio.com"}],
+        "subject": "Filling in the Blanks",
+        "text": "Just a test, hold on until I know which way is up."
       }
     };
 app.get('/', function(req, res, next){
-  /*send email with: function sendTheMail(){
-    m.messages.send(params, function(res){
-      log(res);
+  res.send('Would you like to recieve email notification? Go to "http://localhost:' + port + '/emailme".')
+});
+app.get('/emailme', function(req, res, next){
+  //send email with:
+  //function sendTheMail(){
+    mandrill_client.messages.send(params, function(res){
+      console.log(res);
     }, function(err){
-      log(err);
+      console.log(err);
     });
-  }*/
+  //}
   //return confirmation
   res.send('Email sent');
 });
-var server = app.listen(4242, function(){
-  var host = server.address().address,
-      port = server.address().port;
+var server = app.listen(port, function(){
+  var host = server.address().address;
   console.log('Listening at http://%s:%s', host, port);
 })
