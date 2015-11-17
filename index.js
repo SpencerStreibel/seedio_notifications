905dec26edf42f152fd8ce714d7dda8a56307bd5
spencer <spencer@livioradio.com>
2015-11-17 17:06:04 -0500

    Added SMS Function

--- index.js
+++ index.js
@@ -3,31 +3,42 @@ var express = require('express'),
     mongoose = require('mongoose'),
     mandrill = require('mandrill-api/mandrill'),
     mandrill_client = new mandrill.Mandrill('jx5mDqtrFCdoAkM4o60F-A'), /*or new API key*/
-    port = 4242
+    text = require('textbelt'),
+    port = 4242,
     params = {
       "message": {
-        "from_email": "spencer@livioradio.com",
+        "from_email": "connect@livio.com",
         "to": [{"email":"spencer@livioradio.com"}],
-        "subject": "Filling in the Blanks",
-        "text": "Just a test, hold on until I know which way is up."
+        "subject": "Subject; Change Me",
+        "text": "Body. Change me."
       }
     };
 app.get('/', function(req, res, next){
-  res.send('Would you like to recieve email notification? Go to "http://localhost:' + port + '/emailme".')
-});
-app.get('/emailme', function(req, res, next){
-  //send email with:
-  //function sendTheMail(){
-    mandrill_client.messages.send(params, function(res){
-      console.log(res);
-    }, function(err){
-      console.log(err);
-    });
-  //}
-  //return confirmation
-  res.send('Email sent');
+  res.send('Would you like to recieve email notification? Go to "http://localhost:' + port + '/emailme". \
+  <br>Would you like to recieve text notification? Go to "http://localhost:' + port + '/textme".')
 });
 var server = app.listen(port, function(){
   var host = server.address().address;
   console.log('Listening at http://%s:%s', host, port);
 })
+//Sending email
+app.get('/emailme', function(req, res, next){
+    mandrill_client.messages.send(params, function(res){
+      console.log(res);
+    }, function(err){
+      console.log(err);
+    });
+  res.send('Email sent');
+});
+//Sending text
+app.get('/textme', function(req, res, next){
+  var opts = {
+      fromAddr: 'no-reply@livio.com', //Or other "from" address. Requires 'x@x.x'
+      region: 'us',
+      subject: 'Do not reply' //mandatory subject included as "(subject)"
+  },
+      phonumber = /*Insert Phone Number: XxxXxxXxxx*/;
+  text.sendText(phonumber, 'This is a default message.', opts);
+  console.log('Message sent to ' + phonumber);
+  res.send('Text sent');
+});
