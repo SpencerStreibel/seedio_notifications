89142379a66a008b521b32c6af436354f43a78ba
spencer <spencer@livioradio.com>
2015-12-11 17:01:07 -0500

    Functional module, still alpha

--- notifyseed/index.js
+++ notifyseed/index.js
@@ -1,7 +1,7 @@
-var Notify = function(apikey){
+var Notify = function(apikey) {
   this.apikey = apikey
 }
-Notify.prototype.emailing = function(from, to, sub, body, callback){
+Notify.prototype.emailing = function(from, to, sub, body, callback) {
   var mandrill = require('mandrill-api/mandrill'),
       mandrill_client = new mandrill.Mandrill(this.apikey),
       params = {
@@ -11,24 +11,57 @@ Notify.prototype.emailing = function(from, to, sub, body, callback){
           "subject": sub,
           "text": body
         }
-      };
-      mandrill_client.messages.send(params, function(res){
-        console.log(res);
-        callback(undefined, true)
-      }, function(err){
-        console.log(err);
-        callback(err, false)
-      });
+      },
+      errlog = [];
+
+  mandrill_client.messages.send(params, function(res) {
+    console.log(res);
+    errlog.push(res);
+    switch(res[0]['status']) {
+      case 'invalid':
+        errlog.push('Invalid "to" address.');
+        break;
+      case 'rejected':
+        errlog.push('Message send unable to be completed.');
+        break;
+      case 'sent':
+        errlog = undefined;
+        break;
+      default:
+        console.log('Unhandled error.')
+        errlog.push('An unexpected error has occurred!');
+    };
+    callback(errlog);
+  }, function(err) {
+    console.log('A Mandrill error occurred:');
+    console.log(err);
+    errlog.push(err);
+    switch(err.code) {
+      case 'ENOTFOUND':
+        errlog.push('Connection error.')
+        break;
+      case -1:
+        errlog.push('Invalid API key.')
+        break;
+      case -2:
+        errlog.push('Invalid "from" address.')
+        break;
+      default:
+        console.log('Unhandled error.');
+        errlog.push('An unexpected error occurred!')
+    };
+    callback(errlog);
+  });
 };
-Notify.prototype.texting = function(from,/* you,*/ to, sub, body, callback){
+Notify.prototype.texting = function(from,/* you,*/ to, sub, body, callback) {
   var text = require('textbelt'),
       opts = {
         fromAddr: from,
-        //fromName: you,    //ignored for texts
+        //fromName: you,    //Inoperable for texts
         region: 'us',
         subject: sub
       };
-  text.sendText(to, body, opts, function(){
+  text.sendText(to, body, opts, function() {
     callback()
     console.log('Message sent to ' + to);
   });
--- test.js
+++ test.js
@@ -3,33 +3,35 @@ var express = require('express'),
     //mongoose = require('mongoose'),
     port = 4242,
     NotifySeed = require('./notifyseed');
-app.get('/', function(req, res, next){
-  res.send('Would you like to recieve email notification? Go to "http://localhost:' + port + '/emailme". \
-  <br>Would you like to recieve text notification? Go to "http://localhost:' + port + '/textme".')
+app.get('/', function(req, res, next) {
+  res.send('Would you like to recieve email notification? \
+  Go to "http://localhost:' + port + '/emailme". <br>\
+  Would you like to recieve text notification? Go to "http://localhost:' + port + '/textme".')
 });
-var server = app.listen(port, function(){
+var server = app.listen(port, function() {
   var host = server.address().address;
   console.log('Listening at http://%s:%s', host, port);
 });
 
 //Sending email
-app.get('/emailme', function(req, res, next){
-  var mail = new NotifySeed('jx5mDqtrFCdoAkM4o60F-A'/*Insert API Key*/);
-  mail.emailing('from@x.x', 'to@x.x', 'Subject', 'Body.', function(err){
-    if(err){
-      res.send(false);
+app.get('/emailme', function(req, res, next) {
+  var mail = new NotifySeed('');  /*Insert Mandrill API Key*/
+  mail.emailing('from@x.x', 'to@x.x', 'Subject', 'Body.', function(err) {
+    if(err) {
+      next(err);
+      res.send(err);
     } else {
-      res.send(true);
-    }
+      res.send('Message Sent');
+    };
   });
 });
 
 //Sending text
-app.get('/textme', function(req, res, next){
+app.get('/textme', function(req, res, next) {
   var smsText = new NotifySeed();
-  //extraneous without textbelt update: smsText.texting('from@x.x', 'Your Name (Optional: Defaults to "Textbelt" for email and is ignored in favor of <from@x.x> for text)', XxxToxXxxx/'to@x.x', 'Optional Subject', 'Body.', function(err){!!!});
-  smsText.texting('from@x.x', XxxToxXxxx, 'Optional Subject', 'Body.', function(err){
-    if(err){
+  //extraneous without textbelt update: smsText.texting('from@x.x', 'Your Name (Optional: Defaults to "Textbelt" for email and is ignored in favor of <from@x.x> for text)', XxxToxXxxx/'to@x.x', 'Optional Subject', 'Body.', function(err) {!!!});
+  smsText.texting('from@x.x', XxxToxXxxx, 'Optional Subject', 'Body.', function(err) {
+    if(err) {
       res.send(err);
     } else {
       res.send('Text message was sent.');
