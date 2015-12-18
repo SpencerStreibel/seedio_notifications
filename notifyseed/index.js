2f584fa86c893b632616816fe9e2423b4a28a03b
spencer <spencer@livioradio.com>
2015-12-18 16:30:56 -0500

    Added comments and began retry for connection error

--- notifyseed/index.js
+++ notifyseed/index.js
@@ -1,10 +1,13 @@
 var Notify = function(apikey) {
-  this.apikey = apikey
-}
+      this.apikey = apikey
+    },
+    async = require('async');
+
 Notify.prototype.emailing = function(from, to, sub, body, callback) {
   var mandrill = require('mandrill-api/mandrill'),
-      mandrill_client = new mandrill.Mandrill(this.apikey),
-      params = {
+      mandrill_client = new mandrill.Mandrill(this.apikey);
+  // Establish an array to include all required information substituted in by the user
+  var params = {
         "message": {
           "from_email": from,
           "to": [{"email":to}],
@@ -12,57 +15,115 @@ Notify.prototype.emailing = function(from, to, sub, body, callback) {
           "text": body
         }
       },
-      errlog = [];
+      // Include empty array for error information and description
+      errlog = [],
+      // Include starting point to increment for error&retry counter
+      retries = 0;
 
-  mandrill_client.messages.send(params, function(res) {
+  // Mandrill (mailchimp) has awful API. This sends stuff.
+  mandrill_client.messages.send(params, sendAcknowledged, sendFailed);
+  // Response object returned if send request understood. May still be unable to send
+  function sendAcknowledged(res) {
     console.log(res);
+    // Include potential error or success in error object
     errlog.push(res);
+    // Response object defines a "status" trait.
     switch(res[0]['status']) {
       case 'invalid':
+        // Include brief description of error in error object
         errlog.push('Invalid "to" address.');
         break;
       case 'rejected':
+        // Include brief description of error in error object
         errlog.push('Message send unable to be completed.');
         break;
       case 'sent':
+        // Set error to undefined to indicate no error occurred
         errlog = undefined;
         break;
       default:
+        // Handle any errors not specified
         console.log('Unhandled error.')
         errlog.push('An unexpected error has occurred!');
     };
+    // Return error object
     callback(errlog);
-  }, function(err) {
-    console.log('A Mandrill error occurred:');
+  };
+  // Error object returned if send request failed
+  function sendFailed(err) {
     console.log(err);
+    // Include potential error in error object
     errlog.push(err);
     switch(err.code) {
-      case 'ENOTFOUND':
-        errlog.push('Connection error.')
+      case -1/*'ENOTFOUND'*/:
+        errlog.push('Send fail. Retrying...')
+        // Exponential backoff and retry
+        // Whilst(a, b, c) performs b asynchronously and repeatedly until a is false
+        async.whilst(function() {
+          // Compare number of attempts to a limit
+          maxAttempts = retries < 4;
+          // Recognize successful send
+          sendSuccess = err == undefined;
+          // Stop on either outcome
+          return maxAttempts || sendSuccess
+        },
+        // Function to be repeated until number of attempts equals limit
+        function(callback) {
+          // Use setTimeout to delay next attempt
+          setTimeout(callback, 1000 * Math.pow(2, retries));
+          // Increment retry count
+          retries++;
+          console.log('Failed sends: ' + retries + '/5');
+          // Retry sending (recursively?)
+          
+        },
+        // Close whilst loop
+        function(err) {
+          if(errlog == undefined) {
+            console.log('Send success after ' + retries + ' attempts!');
+          } else {
+            console.log('All attempts used.');
+            // Include brief description of error in error object
+            errlog.push('Connection error.');
+          };
+        })
         break;
       case -1:
-        errlog.push('Invalid API key.')
+        // Include brief description of error in error object
+        errlog.push('Invalid API key.');
         break;
       case -2:
+        // Include brief description of error in error object
         errlog.push('Invalid "from" address.')
         break;
       default:
+        // Handle any errors not specified
         console.log('Unhandled error.');
         errlog.push('An unexpected error occurred!')
     };
+    // Return error object
     callback(errlog);
-  });
+  };
 };
-Notify.prototype.texting = function(from,/* you,*/ to, sub, body, callback) {
+
+// Something Scott added to help me start that I never figured out
+function createDelayMethod(number) {
+  return function() {
+    // do something...
+  };
+};
+
+Notify.prototype.texting = function(from, to, sub, body, callback) {
+  // Define necessary information as variables for user to substitute information
   var text = require('textbelt'),
       opts = {
         fromAddr: from,
-        //fromName: you,    //Inoperable for texts
+        // Can specify region otherwise with 'canada' or else 'intl'
         region: 'us',
         subject: sub
       };
+  // Send the message
   text.sendText(to, body, opts, function() {
-    callback()
     console.log('Message sent to ' + to);
   });
 };
--- test.js
+++ test.js
@@ -2,39 +2,69 @@ var express = require('express'),
     app = express(),
     //mongoose = require('mongoose'),
     port = 4242,
-    NotifySeed = require('./notifyseed');
+    NotifySeed = require('./notifyseed'),
+    server = app.listen(port, function() {
+      var host = server.address().address;
+      console.log('Listening at http://%s:%s', host, port);
+    });
+
+// Set up root
 app.get('/', function(req, res, next) {
   res.send('Would you like to recieve email notification? \
   Go to "http://localhost:' + port + '/emailme". <br>\
   Would you like to recieve text notification? Go to "http://localhost:' + port + '/textme".')
 });
-var server = app.listen(port, function() {
-  var host = server.address().address;
-  console.log('Listening at http://%s:%s', host, port);
-});
 
-//Sending email
+  // Sending email
 app.get('/emailme', function(req, res, next) {
   var mail = new NotifySeed('');  /*Insert Mandrill API Key*/
   mail.emailing('from@x.x', 'to@x.x', 'Subject', 'Body.', function(err) {
     if(err) {
-      next(err);
+      // Handle errors
+      //next(err);
       res.send(err);
     } else {
+      // It worked. Good job. Continue with doing the things.
       res.send('Message Sent');
     };
   });
 });
+/*
+ * from@x.x / to@x.x ) requires "from" -> "[username]" ; e.g. "spencer"
+ *                     requires "@"
+ *                     requires "x.x" -> "[website].[generally "com"]" ; e.g. "gmail.com"
+ *
+ * Subject  ) can be blank, defaults to '(no subject)'
+ *
+ * Body. ) can be blank
+ */
 
-//Sending text
+  // Sending text
 app.get('/textme', function(req, res, next) {
   var smsText = new NotifySeed();
-  //extraneous without textbelt update: smsText.texting('from@x.x', 'Your Name (Optional: Defaults to "Textbelt" for email and is ignored in favor of <from@x.x> for text)', XxxToxXxxx/'to@x.x', 'Optional Subject', 'Body.', function(err) {!!!});
-  smsText.texting('from@x.x', XxxToxXxxx, 'Optional Subject', 'Body.', function(err) {
+  smsText.texting('from@x.x', XxxToxXxxx, 'Subject', 'Body.', function(err) {
     if(err) {
+      // Handle errors
       res.send(err);
+      next(err);
     } else {
+      // It worked. Good job. Continue with your things.
       res.send('Text message was sent.');
     }
   });
 });
+/*
+ * from@x.x ) requires "from" -> "[username]" ; e.g. "spencer"
+ *            requires "@"
+ *            requires "x.x" -> "[website].[generally com]" ; e.g. "gmail.com"
+ *            Tip! Use [Your Phone Number]@[YourCarrier.Endpoint] to provide a phone number
+ *             that will appear as you provide it
+ *
+ * XxxToxXxxx ) destination number input without country calling code (such as +1) and no punctuation
+ *                e.g. the number "1 (440)248-1632" would be input as "4402481632"
+ *
+ * Subject  ) can be blank, appears as first item in text as [Subject] contained
+ *              within parentheses ; e.g. "(Subject) Body."
+ *
+ * Body.  ) can be blank
+ */
